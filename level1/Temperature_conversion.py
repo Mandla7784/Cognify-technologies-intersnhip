221479c316c4c3309f046a5eddde7a6fcc3db0b3
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-17 11:27:45 +0200

    added a static method to convert

--- level1/Temperature_conversion.py
+++ level1/Temperature_conversion.py
@@ -1,7 +1,21 @@
 class TemperatureConverter:
-    pass
+    @staticmethod
+    def convert(temperature ,unit_measurement):
+        # converting F to C
+        if unit_measurement == "F":
+            temperature = (temperature * (9/5))  + 32
+        else:
+            # C to F
+            temperature = (temperature - 32) / (9/5)
+        return temperature
+
+user_temp = int(input('Enter the temp:  '))
+print()
+user_units = input("Enter unit (F or C:  ").upper()
 
 
 
+results =  TemperatureConverter.convert(user_temp , user_units)
+print(f"Answer is {results}")
 
     
