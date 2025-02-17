c34c85248a99c8ac55cbfb8536460bbf7d5283eb
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-17 11:42:11 +0200

    Done for today

--- level1/Email_validator.py
+++ level1/Email_validator.py
@@ -2,20 +2,22 @@ import regex
 
 # functionto check if email is valid ot not 
 def validating_email(email:str) -> str:
-    pattern = 
+    pattern ="^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$"
 
-    if email.split("@")[1].split("gamil")[1] not in string.punctuation:
-        return "Invalid  email"
-
-    elif  "@" not in email:
-        return "Invalid"
-
-    elif email.split("@")[1] is not "gmail.":
-        return "invalid email"
-
+    if  regex.match(pattern , email):
+        return "valid  email"
     else:
-        return "Valid email"
+        return "Invalid "
 
-print(validating_email("mandla@gmai.com"))
+    # elif  "@" not in email:
+    #     return "Invalid"
+
+    # elif email.split("@")[1] is not "gmail.":
+    #     return "invalid email"
+
+    # else:
+    #     return "Valid email"
+
+print(validating_email("mandlagmai.com"))
 
 
