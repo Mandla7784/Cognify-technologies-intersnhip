ad7ac3ed35d6ea6f8ae63041ec04916342d7e102
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-18 09:33:10 +0200

    aaded checked lenght

---
+++ level2/Password_checker.py
@@ -0,0 +1,22 @@
+import regex
+
+
+def password_checker(password:str)  -> str:
+
+    if len(password) < 8:
+        return "Invalid"
+
+    for char in password:
+        
+
+
+    
+
+
+
+def main():
+    ...
+
+
+if __name__=="__main__":
+    main()
