7832b8024264dbc5d8df7dedf28b7b611bfcda1c
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-18 09:07:24 +0200

    removed reversed functionn

--- Palindrome_checker.py
+++ Palindrome_checker.py
@@ -1,4 +1,21 @@
 
 def isPalindrome(string:str) -> bool:
 
-    
+    string_copy = string[::-1]
+
+    if string_copy == string:
+        return True
+
+    return False
+
+
+
+def main():
+    print(isPalindrome("madam"))
+
+
+
+
+        
+if __name__=="__main__":
+    main()
