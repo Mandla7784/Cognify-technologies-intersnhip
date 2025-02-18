f4d23ad591a4613ddf09491869967fe9a4447488
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-18 09:51:53 +0200

    Added password checkr

--- level2/Password_checker.py
+++ level2/Password_checker.py
@@ -1,22 +1,41 @@
-import regex
+import string
 
 
+capitalalphabets= string.ascii_uppercase
+smallalphabets= string.ascii_letters
+specialchar=string.punctuation
+digits=string.digits
+
+
+
+print(string.punctuation)
 def password_checker(password:str)  -> str:
+ 
+    special = False
+    uppercase = False
+    lowercase =False
+    number = False
+
+
 
     if len(password) < 8:
         return "Invalid"
 
     for char in password:
-        
 
+        if char in capitalalphabets:uppercase = True
+        elif char in smallalphabets:lowercase =True
+
+        elif char in specialchar:special  = True
+        elif char in digits: number = True
+
+    return     special , uppercase , lowercase , number
+
+      
 
     
-
-
-
-def main():
-    ...
-
+def main(): 
+    print(password_checker("w@@@MANDLA45"))
 
 if __name__=="__main__":
     main()
