cea5fbe4ba210928bd2bb0f85efad11605dfdb62
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-19 11:09:00 +0200

    working on bugs

--- level3/Data_visualzation.py
+++ level3/Data_visualzation.py
@@ -3,19 +3,31 @@ import matplotlib.pyplot
 
 
 
-
-
-
 class DataVisulaizingTool:
-    pass
- 
+    def __init__(self , file) -> None:
+        self.file = file
+       
 
 
+    def get_data(self) -> object:
+        data = pd.read_csv(self.file)
+        return data
+    
+    # histogram of teenagers
+
+    def histogram(self , func)-> None:
+    
+
+        matplotlib.pyplot.hist(func['Range_preg'])
+        matplotlib.pyplot.title("Teenage pregnancy")
+        matplotlib.pyplot.show()
 
 
 
 def main():
-    pass
+    vfx_Tool =  DataVisulaizingTool("Teenage_pregnancy.csv")
+    # showing the graph
+    vfx_Tool.histogram(DataVisulaizingTool.get_data(vfx_Tool))
 
 
 
