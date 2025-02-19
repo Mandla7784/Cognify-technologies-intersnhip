653875d10c0c0de01d837ab37d75b097b0b71937
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-19 10:05:32 +0200

    Updates

--- level2/file_handling.py
+++ level2/file_handling.py
@@ -1,21 +1,34 @@
 def file_handler(file_name):
     try:
         with open(file_name , "r") as file:
-            content = file.readlines()
-            return content
+            content = file.read().lower()
+            words = content.split()
+
+        return words
+
+   
 
     except FileExistsError:
-        return 
+
+        print(f"Error: {file_name} not found.")
+        return []
    
 
 def main():
-    content = file_handler("names.txt")
- 
-    for word  in content:
-        print(word)
-    
+    # counting occurrences of each word
+    words  = file_handler("names.txt")
+    word_count   = {}
 
-    
+    for word in words:
+        word_count[word] = word_count.get(word , 0 ) + 1
+
+    my_words =dict(sorted(word_count.items()))
+
+
+    for word ,count in my_words.items():
+        print(f"{word} : {count}")
+
+
 
 if __name__=="__main__":
     main()
