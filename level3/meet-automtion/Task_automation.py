5a44930c8b13dd2fe75c44f54569462a4a638021
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-20 10:37:49 +0200

    Added secrete json file

--- level3/meet-automtion/Task_automation.py
+++ level3/meet-automtion/Task_automation.py
@@ -15,27 +15,39 @@ from google.auth.transport import requests
 
 # authorization 
 
-def authorize()-> Credentials:
-    client_secrete_file = "./client_secret.json"
+def authorize() -> Credentials:
+    """Ensure valid credentials for calling the Meet REST API."""
+    client_SECRET_FILE = "./client_secret.json"
     credentials = None
 
-    if os.path.exists("tokes.json"):
-        credentials = Credentials.from_authorized_user_file("token.json")
-
+    if os.path.exists('token.json'):
+        credentials = Credentials.from_authorized_user_file('token.json')
 
     if credentials is None:
         flow = InstalledAppFlow.from_client_secrets_file(
-             client_secrete_file,
-             scopes=[
-                 "https://www.googleapis.com/auth/meetings.space.created"
-             ]
-        )
+         client_SECRET_FILE ,
+            scopes=[
+                'https://www.googleapis.com/auth/meetings.space.created',
+            ])
+        flow.run_local_server(port=0)
+        credentials = flow.credentials
+
+    if credentials and credentials.expired:
+        credentials.refresh(requests.Request())
+
+    if credentials is not None:
+        with open("token.json", "w") as f:
+            f.write(credentials.to_json())
+
+    return credentials
 
 
 
 
 def main():
-    ...
+
+    USER_CREDENTIALS = authorize()
+
 
 if __name__=="__main__":
-    main()
+    main() 
