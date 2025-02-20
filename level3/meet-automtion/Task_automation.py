6afd37d7ab58b658cd8c12257233b6c5c11622ba
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-02-20 11:15:16 +0200

    created space for my meeting

--- level3/meet-automtion/Task_automation.py
+++ level3/meet-automtion/Task_automation.py
@@ -1,6 +1,6 @@
 """
     In this project we create a program that will automate the process of joing metting with google meet
-    we use google cloud perhaps for the project database services
+    we use google cloud perhaps for the project database services / actually not for DB but for its functions
 """
 
 import os
